dbfd75a880cdf159f3296dc3daa3a3cae413c3a0
Thomas Buscaglia <t.buscaglia@xsolla.com>
2024-02-20 22:43:09 -0800

    WIP cjs cli update

--- test/NodejsServerGenerator.test.ts
+++ test/NodejsServerGenerator.test.ts
@@ -10,24 +10,22 @@ describe("NodejsServerGenerator Tests", () => {
     });
 
     afterAll(() => {
-        //rimraf.sync(path.join(__dirname, "tmp"));
+        rimraf.sync(path.join(__dirname, "tmp"));
     });
 
-    it("Can generator project successfully.", async (done: Function) => {
+    it("Can generator project successfully.", async () => {
         const apiSpec: any = await OASUtils.loadSpec(path.join(__dirname, "petstore.yaml"));
         expect(apiSpec).toBeDefined();
 
         // Generate the project
         const generator: NodejsServerGenerator = new NodejsServerGenerator(Logger());
-        generator.init();
+        generator.init(path.join(__dirname, "..", "templates"));
         await generator.generate(apiSpec, path.join(__dirname, "tmp"), []);
 
-        // TODO Remove this delay once we figure out why the generator isn't completing after the wait
-        setTimeout(() => {
-            // Now scan the resulting directory to see if it matches our sample
-            const result: compare.Result = compare.compareSync(path.join(__dirname, "nodejs_server_petstore"), path.join(__dirname, "tmp"), { compareContent: true, excludeFilter: "**/*.md" });
-            expect(result.same).toBeTruthy();
-            done();
-        }, 1000);
+        // Now scan the resulting directory to see if it matches our sample
+        const result: compare.Result = await compare.compare(path.join(__dirname, "nodejs_server_petstore"), path.join(__dirname, "tmp"), { compareContent: true, excludeFilter: "**/*.md" });
+        expect(result.same).toBeTruthy();
+    });
+
     });
 });
